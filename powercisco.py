9500285700a9570afec19b9675dadf1e2e8d9981
napalm255 <napalm255@gmail.com>
2016-02-10 02:30:00 -0500

    overhaul

--- powercisco.py
+++ powercisco.py
@@ -11,22 +11,7 @@ import time
 import datetime
 from ciscoconfparse import CiscoConfParse
 
-# application information
-class app ( object ) :
-    def __init__ ( self ) :
-        self.name = 'powercisco'
-        self.version = '0.1'
-        self.filename = os.path.basename(__file__)
-        self.path = os.path.realpath(__file__).replace(self.filename,'')
-        self.debugfile = self.path + self.name + '.debug'
-        self.devicepath = self.path + 'devices/'
-        if not os.path.exists(self.devicepath):
-            os.makedirs(self.devicepath)
-
-        print('=> ' + self.name + ' v' + self.version)
-
-        return
-
+# debug
 class debug ( object ) :
     def __init__ ( self ) :
         self.enabled = 0
@@ -45,17 +30,17 @@ class debug ( object ) :
 
         return
 
-    def enable ( self ) :
+    def enable ( self, debugfile ) :
         self.enabled = 1
-        if os.path.exists(app.debugfile):
-            self.logfile = open(app.debugfile, 'a')
-            self.log(':: debug mode enabled')
-            self.log(':: debug log already exists')
-            self.log(':: time: ' + datetime.datetime.now().isoformat())
-            self.log(':: sys: ' + sys.version.replace('\n', '\n        '))
+        if os.path.exists(debugfile):
+            self.logfile = open(debugfile, 'a')
         else:
-            self.logfile = open(app.debugfile, 'w')
-            self.log(':: debug log does not exist')
+            self.logfile = open(debugfile, 'w')
+
+        self.log('<debug mode>')
+        self.log(':: time: ' + datetime.datetime.now().isoformat())
+        self.log(':: sys: ' + sys.version.replace('\n', '\n        '))
+        self.log('</debug mode>')
 
         return
 
@@ -64,35 +49,7 @@ class debug ( object ) :
 
         return
 
-# config handler
-class config ( object ) :
-    data = {}
-
-    def __init__ ( self ) :
-        return
-
-    def load ( self, filename ) :
-        self.data.update(json.loads(open(filename).read()))
-
-        return self.data
-
-    def new ( self, type, filename ) :
-        if 'app' in type :
-            data = {'config':[
-                     {'device_path':'/devices'}
-                   ]}
-        elif 'dev' in type :
-            data = {'devices':[
-                     {'host':'device1','user':'','pass':'','groups':['dc1','cisco'],'compliance':['all']},
-                     {'host':'device2','user':'','pass':'','groups':['dc2','cisco'],'compliance':['all']}
-                   ]}
-
-        with open(filename, 'w') as outfile:
-            json.dump(data, outfile, sort_keys=True, indent=2, separators=(',', ': '))
-
-        return
-
-# ssh handler
+# ssh
 class ssh ( object ) :
 
     def __init__ ( self ) :
@@ -102,7 +59,7 @@ class ssh ( object ) :
         if os.path.exists(self.user_config_file):
             with open(self.user_config_file) as f:
                 self.ssh_config.parse(f)
-        self.localpath = app.devicepath
+        self.localpath = powercisco.devicepath
         if not os.path.exists(self.localpath):
             os.makedirs(self.localpath)
 
@@ -137,7 +94,7 @@ class ssh ( object ) :
         while self.channel.recv_ready():
             out = out + str(self.channel.recv(1024))
 
-        ret = {'args':args, 
+        ret = {'args':powercisco.args, 
                'user_config_file':self.user_config_file,
                'ssh_config':host,
                'login_output':out
@@ -148,7 +105,7 @@ class ssh ( object ) :
     # TODO: support SSH and saving config
     # TODO: add download validation
     def download ( self, device, filename ) :
-        localpath = app.devicepath + device['host']
+        localpath = powercisco.devicepath + device['host']
         if not os.path.exists(localpath):
             os.makedirs(localpath)
 
@@ -179,130 +136,218 @@ class ssh ( object ) :
 
         return ret
 
-#### functions ####
-
-# authentication handler
-#    lookup/combine credentials from different sources
-# TODO: support key
-def handler_auth ( device ) :
-    ssh_config = paramiko.SSHConfig()
-    user_config_file = os.path.expanduser('~/.ssh/config')
-    if os.path.exists(user_config_file):
-        with open(user_config_file) as f:
-            ssh_config.parse(f)
-
-    host = ssh_config.lookup(device['host'])
-
-    ret = { 'user':'', 'pass':'' }
-    if args.u:
-        ret['user'] = args.u[0]
-    elif 'user' in host:
-        ret['user'] = host['user']
-    else:
-        msg = 'missing username'
-        if 'error' in ret:
-            ret['error'] = ret['error'] + '; ' + msg
-        else:
-            ret['error'] = msg
-
-    if args.p:
-        ret['pass'] = args.p[0]
-    else:
-        msg = 'missing password or key'
-        if 'error' in ret:
-            ret['error'] = ret['error'] + '; ' + msg
-        else:
-            ret['error'] = msg
-
-    return ret
-
-# device handler
-#    lookup/combine devices
-def handler_devices ( ) :
-    ret = ''
-    # load device list
-    if args.devices:
-        print(':: loading device list')
-        for device in args.devices:
-            if not ret:
-                ret = [{'host':device}]
-            else:
-                ret.append({'host':device})
-
-    # load device configuration
-    if args.config:
-        if 'dev' in args.config:
-            print(':: loading device config')
-            cfg = config()
-            cfg.load('dev.json')
-            for device in cfg.data['devices']:
-                if not ret:
-                    ret = [device]
-                else:
-                    ret.append(device)
-
-    # filter for groups
-    # TODO: add actual filter
-    if args.groups:
-        print(':: filtering groups')
-        print(args.groups)
-
-    return ret
-
 # handler
-#   what to run and in what order
-# TODO: create handler class and merge auth and devices
-def handler ( ) :
-    output = {}
+class powercisco ( object ) :
 
-    # enable stdout logging
-    if args.log and args.log != '-':
-        sys.stdout = open(args.log, 'w')
+    def __init__ ( self ) :
+        self.name = 'powercisco'
+        self.version = '0.2'
+        self.filename = os.path.basename(__file__)
+        self.path = os.path.realpath(__file__).replace(self.filename,'')
+        self.cfg = {
+                     'debug_file':self.path + self.name + '.debug',
+                     'device_path':self.path + 'devices/',
+                     'ssh_config':'~/.ssh/config'
+                   }
+        self.dev = []
+        self.dev_config = []
+        self.dev_group = []
+        self.dev_list = []
+
+        self.configfile = self.path + self.name + '.json'
+        self.debugfile = self.path + self.name + '.debug'
+        self.devicepath = self.path + 'devices/'
+        self.sshconfig = '~/.ssh/config'
+
+        # create directories
+        if not os.path.exists(self.devicepath):
+            os.makedirs(self.devicepath)
+
+        # handle arguments
+        self.args = self.arguments()
+
+        # enable stdout logging
+        if self.args.log and self.args.log != '-':
+            sys.stdout = open(self.args.log, 'w')
     
-    # enable debugging
-    if args.debug:
-        debug.enable()
-    
-    # generate new config from template
-    if args.new:
-        # TODO:check if already exists
-        newcfg = config()
-        for cfg in args.new:
-            print(':: generating new ' + cfg + ' config')
-            newcfg.new(cfg, cfg + '.json')
-        newcfg = None
-        print(':: finished')
+        # print banner
+        print('=> ' + self.name + ' v' + self.version)
+
+        # handle debug
+        dbug = debug()
+        if self.args.debug:
+            dbug.enable(self.debugfile)
+
+    # argument handler
+    def arguments ( self ) :
+        # TODO: add ssh key support
+        parser = argparse.ArgumentParser(
+                            description=self.name + 
+                            ' v' + self.version + 
+                            ' - managing cisco devices',
+                            prog='tool',
+                            formatter_class=lambda prog:
+                              argparse.HelpFormatter(prog,max_help_position=50)
+                          )
+        parser.add_argument('-u', nargs=1,
+                           help='username')
+        parser.add_argument('-p', nargs=1,
+                           help='password')
+        #parser.add_argument('-k', nargs=1,
+        #                   help='ssh key file')
+        parser.add_argument('-r', nargs='+',
+                           help='command(s) to run')
+        parser.add_argument('-d', nargs='+', choices=['run', 'start', 'tech'],
+                           help='download cisco configuration(s)')
+        parser.add_argument('-c', nargs='+',
+                           help='cisco configuration file(s)')
+        parser.add_argument('-s', choices=['run', 'start', 'tech'],
+                           help='show cisco configuration(s)')
+        parser.add_argument('--devices', nargs='+',
+                           help='device(s)')	
+        parser.add_argument('--groups', nargs='+',
+                           help='group(s)')
+        parser.add_argument('--config', nargs='+', choices=['app', 'dev'],
+                           help='load json config(s)')
+        parser.add_argument('--new', nargs='+', choices=['app', 'dev'],
+                           help='new json config(s)')
+        parser.add_argument('--debug', action='store_true',
+                           help='enable debug')
+        parser.add_argument('--out', nargs='?',
+                           help='output to file')
+        parser.add_argument('--log', nargs='?',
+                           default='-',
+                           help='log to file')
+        
+        if len(sys.argv) == 1:
+            parser.print_help()
+            sys.exit(1)
+        
+        args = parser.parse_args()
+        
+        return args
+
+    # handler
+    # TODO: error is now an array. need to handle accordingly
+    def handler ( self ) :
+        print(self.config('new', self.configfile))
+        print(self.config('load', self.configfile))
+        print(self.devices())
+
+        output = {}
+        devices = self.devices()
+        for device in devices:
+            print('>> device: ' + device['host'])
+            output[device['host']] = {}
+
+            # no auth required
+            #if 'error' in device:
+            #    print(':: error: ' + str(device['error']) + '. skipping.')
+            #    continue
+            #elif self.args.s:
+            #    output[device['host']]['show'] = config_show(device, self.args.s)
+            #    print(output[device['host']]['show'])
+
+            # auth required
+            creds = self.auth(device)
+            device.update(creds)
+            #if 'error' in device:
+            #    print(':: error: ' + str(device['error']) + '. skipping.')
+            #    continue
+            if self.args.r:
+                output[device['host']]['run'] = run_commands(device, self.args.r)
+            elif self.args.d:
+                output[device['host']]['download'] = config_download(device, self.args.d)
+
         return
 
-    # loop through devices
-    # TODO: need better way of handling order and requirements for actions
-    #       a function with a list of args and their required args
-    # TODO: remove repeated code
-    devices = handler_devices()
-    for device in devices:
-        print('>> device: ' + device['host'])
-        output[device['host']] = {}
+    def config (self, action, filename ) :
+        ret = {}
+        if 'load' in action:
+            try:
+                config_load = json.loads(open(filename).read())
+                if 'config' in config_load:
+                    self.cfg.update(config_load['config'])
+                if 'devices' in config_load:
+                    self.dev_config = config_load['devices']
+            except:
+                ret = {'error':'failed to load configuration'}
 
-        # no auth required
-        if 'error' in device:
-            print(':: error: ' + device['error'] + '. skipping.')
-            continue
-        elif args.s:
-            output[device['host']]['show'] = config_show(device, args.s)
-            print(output[device['host']]['show'])
+        elif 'new' in action:
+            try:
+                data = {'config':{
+                         'debug_file':self.path + self.name + '.debug',
+                         'device_path':self.path + 'devices/',
+                         'ssh_config':'~/.ssh/config'
+                        },'devices':[
+                          {'host':'device1','user':'','pass':'','groups':['dc1','cisco'],'compliance':['all']},
+                          {'host':'device2','user':'','pass':'','groups':['dc2','cisco'],'compliance':['all']}
+                        ]}
 
-        # auth required
-        creds = handler_auth(device)
-        device.update(creds)
-        if 'error' in device:
-            print(':: error: ' + device['error'] + '. skipping.')
-            continue
-        elif args.r:
-            output[device['host']]['run'] = run_commands(device, args.r)
-        elif args.d:
-            output[device['host']]['download'] = config_download(device, args.d)
+                with open(filename, 'w') as outfile:
+                    json.dump(data, outfile, sort_keys=True, indent=2, separators=(',', ': '))
+            except:
+                ret = {'error':'failed to create configuration'}
 
-    return output
+        return ret
+
+    def auth ( self, device ) :
+        # TODO: return {'user':'','pass':'','error':''}
+        # TODO: user order - cli, json, sshconfig 
+        # TODO: pass order - cli, json
+        # TODO: support ssh key
+        ssh_config = paramiko.SSHConfig()
+        user_config_file = os.path.expanduser(self.sshconfig)
+        if os.path.exists(user_config_file):
+            with open(user_config_file) as f:
+                ssh_config.parse(f)
+
+        host = ssh_config.lookup(device['host'])
+
+        ret = { 'user':'', 'pass':'', 'error':[] }
+        if self.args.u:
+            ret['user'] = self.args.u[0]
+        elif 'user' in host:
+            ret['user'] = host['user']
+        else:
+            ret['error'].append('missing username')
+
+        if self.args.p:
+            ret['pass'] = self.args.p[0]
+        else:
+            ret['error'].append('missing password or key')
+
+        return ret
+
+    def devices ( self ) :
+        # TODO: return {'host'}
+        # TODO: combine devices - cli, json
+        # TODO: handle duplicates
+        ret = {}
+
+        # list is always added
+        if self.args.devices:
+            for device in self.args.devices:
+               self.dev_list.append({'host':device})
+            self.dev = self.dev_list
+
+        # group requires config
+        # group is more grainular than config
+        if self.args.groups:
+            for group in self.args.groups:
+                for device in self.dev_config:
+                    if group in device['groups']:
+                        self.dev_group.append(device)
+            self.dev += self.dev_group
+        elif self.dev_config:
+            self.dev += self.dev_config
+
+        ret = self.dev
+
+        return ret
+
+#### functions ####
+# TODO: merge all functions under powercisco class
 
 # save file
 def save_file ( filename, data ) :
@@ -331,7 +376,7 @@ def run_commands ( device, cmds ) :
 # download cisco configuration
 def config_download ( device, configs ) :
     print(':: downloading configuration')
-    localpath = app.devicepath + device['host']
+    localpath = powercisco.devicepath + device['host']
     if not os.path.exists(localpath):
         os.makedirs(localpath)
 
@@ -360,7 +405,7 @@ def config_show ( device, config ) :
     if 'start' in config: config = 'startup-config'
     if 'tech' in config: config = 'show-tech'
 
-    localfile = app.devicepath + device['host'] + '/' + config
+    localfile = powercisco.devicepath + device['host'] + '/' + config
     ret = None
 
     if not os.path.exists(localfile):
@@ -372,59 +417,5 @@ def config_show ( device, config ) :
 
     return ret
 
-#### initialization ####
-
-# load application information
-app = app()
-debug = debug()
-
-# argument handler
-# TODO: add key support
-parser = argparse.ArgumentParser(
-                    description=app.name + 
-                    ' v' + app.version + 
-                    ' - managing cisco devices',
-                    prog='tool',
-                    formatter_class=lambda prog:
-                      argparse.HelpFormatter(prog,max_help_position=50)
-                  )
-parser.add_argument('-u', nargs=1,
-                   help='username')
-parser.add_argument('-p', nargs=1,
-                   help='password')
-#parser.add_argument('-k', nargs=1,
-#                   help='ssh key file')
-parser.add_argument('-r', nargs='+',
-                   help='command(s) to run')
-parser.add_argument('-d', nargs='+', choices=['run', 'start', 'tech'],
-                   help='download cisco configuration(s)')
-parser.add_argument('-c', nargs='+',
-                   help='cisco configuration file(s)')
-parser.add_argument('-s', choices=['run', 'start', 'tech'],
-                   help='show cisco configuration(s)')
-parser.add_argument('--devices', nargs='+',
-                   help='device(s)')	
-parser.add_argument('--groups', nargs='+',
-                   help='group(s)')
-parser.add_argument('--config', nargs='+', choices=['app', 'dev'],
-                   help='load json config(s)')
-parser.add_argument('--new', nargs='+', choices=['app', 'dev'],
-                   help='new json config(s)')
-parser.add_argument('--debug', action='store_true',
-                   help='enable debug')
-parser.add_argument('--out', nargs='?',
-                   help='output to file')
-parser.add_argument('--log', nargs='?',
-                   default='-',
-                   help='log to file')
-
-if len(sys.argv) == 1:
-    parser.print_help()
-    sys.exit(1)
-
-args = parser.parse_args()
-
-# handle it
-ret = handler()
-#if ret:
-#    print(ret)
+powercisco = powercisco()
+powercisco.handler()
